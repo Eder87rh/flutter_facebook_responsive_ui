311000fce3e125d427284d2f410d373f2d0fc10d
Eder <eder.ramirez87@gmail.com>
2020-08-22 18:08:37 -0700

    displaying facebook in SliverAppBar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../config/palette.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: TextStyle(
+                color: Palette.facebookBlue,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
